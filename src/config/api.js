0cb70c7b6afaab6aa1dabbf564b4fd17fee0ad63
aditiakurniawan <108774796+aditiakurniawan@users.noreply.github.com>
2022-10-26 01:20:47 +0700

    confiq

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: process.env.REACT_APP_BASEURL,
+  baseURL: "https://nutek.herokuapp.com/api/v1/",
 });
 
 export const setAuthToken = (token) => {
